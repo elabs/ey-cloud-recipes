024fba4f819c4eceb0041888772e7d0a49ee1d82
Anders Carlsson and Anders Törnqvist <dev+ankor+unders@elabs.se>
2012-09-19 10:57:37 +0200

    FFMPEG: install binary

--- cookbooks/ffmpeg/recipes/default.rb
+++ cookbooks/ffmpeg/recipes/default.rb
@@ -1,27 +1,42 @@
-#
-# Cookbook Name:: ffmpeg
-# Recipe:: default
-#
-# Author:: Jamie Winsor (<jamie@vialstudios.com>)
-#
-# Copyright 2011, En Masse Entertainment, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+remote_file "/usr/local/bin/ffmpeg" do
+  source "http://streamio-system-downloads.s3.amazonaws.com/ubuntu-12.04/ffmpeg-0.11.1"
+  checksum "0a31df19446917301976d90cace9145f"
+  mode 0755
+  owner "root"
+  group "root"
+end
 
-case node[:ffmpeg_install_method]
-when :source
-  include_recipe "ffmpeg::source"
-when :package
-  include_recipe "ffmpeg::package"
+remote_file "/usr/local/bin/qt-faststart" do
+  source "http://streamio-production-us.s3.amazonaws.com/system-downloads/qtfaststart.py"
+  checksum "cbfbb84b05988e37f45b23a32bfbb40c"
+  mode 0755
+  owner "root"
+  group "root"
+end
+
+remote_file "/etc/ffmpeg.tar.gz" do
+  source "http://streamio-production-us.s3.amazonaws.com/system-downloads/ffmpeg.tar.gz"
+  checksum "1499b44e745db478fb61c155a2fd272f"
+  mode 0755
+  owner "root"
+  group "root"
+  not_if { File.directory?("/etc/ffmpeg") }
+end
+
+bash "untar ffmpeg presets" do
+  user "root"
+  cwd "/etc"
+  code "tar xvfz ffmpeg.tar.gz && rm ffmpeg.tar.gz"
+  only_if { File.exists?("/etc/ffmpeg.tar.gz") }
+end
+
+user_dirs = Dir.glob("/home/*")
+
+raise "No user dirs found for .ffmpeg presets which can cause havoc later on!" if user_dirs.empty?
+
+user_dirs.each do |path|
+  link "#{path}/.ffmpeg" do
+    to "/etc/ffmpeg"
+    not_if { File.symlink?("#{path}/.ffmpeg") }
+  end
 end
